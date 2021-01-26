f82dcbe91e62312f320e24608d390ba176328606
sheenhill <1050844206@qq.com>
2021-01-26 22:48:29 +0800

    commit test

--- common/src/main/java/com/sheenhill/common/binding_adapter/ImgBindingAdapter.kt
+++ common/src/main/java/com/sheenhill/common/binding_adapter/ImgBindingAdapter.kt
@@ -5,7 +5,7 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
-@BindingAdapter(value = [ "url"])
+@BindingAdapter(value = ["url"])
 fun showImgByGlide(imgView: ImageView, url: String) {
     Glide.with(imgView).load(url).into(imgView)
 }
